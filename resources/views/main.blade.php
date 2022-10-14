83bfd5720623eb8451005bf54435fcda74b2d133
librankaio <rafiqzayana@gmail.com>
2022-10-14 10:23:17 +0700

    favico image

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>BENINDOS</title>
+    <title>BENINDO REPORTING</title>
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
